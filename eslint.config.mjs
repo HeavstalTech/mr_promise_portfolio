7c05a2d84d0ef44d4855dcec0b1ce6e8f96e9a2c
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-07-11 19:57:31 +0100

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// eslint.config.mjs
+export default [
+  {
+    ignores: [".next/**", "node_modules/**", "public/**"],
+  },
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    rules: {
+    },
+  },
+];
